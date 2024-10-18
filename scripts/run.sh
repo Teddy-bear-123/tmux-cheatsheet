8c2528f7cfc13cda9052c764c75df5e5a1d71308
teddy <plsbetmax69420@gmail.com>
2024-10-19 02:31:45 +0530

    commit

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,8 +4,14 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Path to the Rust binary
-BINARY_PATH="$CURRENT_DIR/../target/debug/tmux-cheetsheet"
+BINARY_PATH="$CURRENT_DIR/../target/release/tmux-cheatsheet"
 
-# Run the Rust binary to display the cheatsheet
-"$BINARY_PATH"
+# Check if the Rust binary exists
+if [[ -f "$BINARY_PATH" ]]; then
+    # Run the Rust binary to display the cheatsheet
+    "$BINARY_PATH" && notify-send "Tmux Cheatsheet" "Cheatsheet displayed"
+else
+    echo "Error: Tmux cheatsheet binary not found at $BINARY_PATH"
+    notify-send "Error" "Tmux cheatsheet binary not found"
+fi
 
